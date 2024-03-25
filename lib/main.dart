43bc25de1b2d3fd78c9fb0cd99c8444bec891dd3
wdg <wdg980813@gmail.com>
2024-03-25 18:31:17 +0900

    환경 세팅 및 StatefulWidget, StatelessWidget

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HelloPage('이건가'),
+    );
+  }
+}
+
+class HelloPage extends StatefulWidget {
+  final String title;
+
+  HelloPage(this.title, {super.key});
+
+  @override
+  State<HelloPage> createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  String _message = '호달달';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _changeMessage,
+        ),
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+          ),
+        ),
+        body: Text(
+          _message,
+          style: TextStyle(fontSize: 40),
+        ));
+  }
+
+  void _changeMessage() {
+    setState(() {
+      _message = '크크크';
+    });
+  }
+}
